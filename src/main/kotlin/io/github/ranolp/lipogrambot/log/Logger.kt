f360a1825c287806dd5339626c47e3b79306d446
Ranol <asdhugh1@gmail.com>
2017-10-09 21:22:03 +0900

    Add logger

---
+++ src/main/kotlin/io/github/ranolp/lipogrambot/log/Logger.kt
@@ -0,0 +1,54 @@
+package io.github.ranolp.waffle.log
+
+import io.github.ranolp.lipogrambot.general.options.CoreOption
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+object Logger {
+    enum class LogLevel(val level: Int) {
+        ALL(Integer.MIN_VALUE),
+        VERBOSE(-1),
+        INFO(0),
+        WARNING(1),
+        ERROR(2),
+        OFF(Integer.MAX_VALUE)
+    }
+
+    private var formatter: DateTimeFormatter? = null
+    val logLevel: LogLevel by lazy {
+        LogLevel.values().first { it.name == CoreOption.logLevel.toUpperCase() }
+    }
+
+    private fun log(level: LogLevel, text: String) {
+        if (Logger.logLevel.level > level.level) {
+            return
+        }
+        val now = LocalDateTime.now()
+        if (formatter === null) {
+            formatter = DateTimeFormatter.ofPattern(CoreOption.dateSyntax)
+        }
+        println("[${now.format(formatter)}/$level] $text")
+    }
+
+    fun verbose(text: String) {
+        log(LogLevel.VERBOSE, text)
+    }
+
+    fun info(text: String) {
+        log(LogLevel.INFO, text)
+    }
+
+    fun warning(text: String) {
+        log(LogLevel.WARNING, text)
+    }
+
+    fun error(text: String) {
+        log(LogLevel.ERROR, text)
+    }
+
+    fun error(throwable: Throwable) {
+        log(LogLevel.ERROR,
+                (throwable::class.simpleName + ": " + throwable.message + throwable.stackTrace.joinToString { "\n  at ${it.className}.${it.methodName}" + if (it.isNativeMethod) "(Native Method)" else "(${it.fileName}:${it.lineNumber})" }).color(
+                        text = Text.RED))
+    }
+}
