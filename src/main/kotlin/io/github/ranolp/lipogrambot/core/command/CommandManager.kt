67dba996e349946ae464cf8bc1988fc37367f1b0
Ranol <asdhugh1@gmail.com>
2017-10-09 21:19:14 +0900

    Add command core

---
+++ src/main/kotlin/io/github/ranolp/lipogrambot/core/command/CommandManager.kt
@@ -0,0 +1,26 @@
+package io.github.ranolp.lipogrambot.core.command
+
+import io.github.ranolp.lipogrambot.core.layer.LipogramGroup
+import io.github.ranolp.lipogrambot.core.layer.LipogramUser
+
+object CommandManager {
+    private val commands = mutableMapOf<String, Command>()
+
+    @JvmName("registerCommand") operator fun plusAssign(command: Command) {
+        command.aliases.forEach {
+            commands += it to command
+        }
+    }
+
+    @JvmName("unregisterCommand") operator fun minusAssign(label: String) {
+        commands -= label
+    }
+
+    @JvmName("unregisterCommand") operator fun minusAssign(command: Command) {
+        commands -= command.aliases
+    }
+
+    fun dispatchCommand(group: LipogramGroup, runner: LipogramUser, label: String, arguments: List<String>) {
+        commands[label]?.run(group, runner, label, arguments)
+    }
+}
